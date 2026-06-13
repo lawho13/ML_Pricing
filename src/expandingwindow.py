5a1053e3ceab5249460b3c17fca2365649f79854
Lawrence Ho <lawrenceho306@gmail.com>
2026-06-13 02:48:01 -0400

    add FF dataset processing to preprocess script, expandingwindow script updated, update readme and requirements

--- src/expandingwindow.py
+++ src/expandingwindow.py
@@ -9,42 +9,58 @@ class ExpandingWindow:
         self.val_years = val_years
         self.test_years = test_years
 
-    def split(self, df, date_col='DATE'):
-
-        # sort all rows in the dataframe by their dates
+    def split_paper(df, initial_train_years=18, val_years=12, test_years=1, date_col="DATE"):
+    
         df = df.sort_values(date_col).reset_index(drop=True)
 
         start_date = df[date_col].min()
         end_date   = df[date_col].max()
-
-        # let the current train_end be start_date plus the arg from the constructor for train years
-
-        train_end = start_date + pd.DateOffset(years=self.initial_train_years)
-
+        train_end = start_date + pd.DateOffset(years=initial_train_years)
+        
         while True:
-            # the backtesting loop - here, we set val_end to the date train_end + val from constructor, likewise with test_end
-            val_end  = train_end + pd.DateOffset(years=self.val_years)
-            test_end = val_end   + pd.DateOffset(years=self.test_years)
+            val_start = train_end
+            val_end   = val_start + pd.DateOffset(years=val_years)
 
-            # if this is the case, we've exceeded window length - break
+            test_start = val_end
+            test_end   = test_start + pd.DateOffset(years=test_years)
+
             if test_end > end_date:
                 break
-            
-            # these are all the valid data frames to return - we have a training dataframe, a validation data frame, and a test data frame - all within their respective date timelines
-            train_df = df[df[date_col] <  train_end]
-            val_df   = df[(df[date_col] >= train_end) & (df[date_col] < val_end)]
-            test_df  = df[(df[date_col] >= val_end)   & (df[date_col] < test_end)]
 
-            # in tuple format, return the data frames
+            train_df = df[df[date_col] < train_end]
+
+            val_df = df[
+                (df[date_col] >= val_start) &
+                (df[date_col] <  val_end)
+            ]
+
+            test_df = df[
+                (df[date_col] >= test_start) &
+                (df[date_col] <  test_end)
+            ]
+
+            print(
+                f"Train: {train_df[date_col].min().date()} → "
+                f"{train_df[date_col].max().date()} | "
+                f"Val: {val_df[date_col].min().date()} → "
+                f"{val_df[date_col].max().date()} | "
+                f"Test: {test_df[date_col].min().date()} → "
+                f"{test_df[date_col].max().date()}"
+            )
+
             yield train_df, val_df, test_df
 
-            # set train_end to test_end to adjust the window and move the time series window up for another testing round
-            train_end = test_end
+            # move forward ONE year only
+            train_end = train_end + pd.DateOffset(years=1)
 
     # this method gives us X - input variables - and Y - target variables, for any dataframe
     @staticmethod
-    def get_features_and_target(df, target_col='ret'):
-        drop_cols = ['DATE', 'year_month', 'permno', target_col]
-        X = df.drop(columns=[c for c in drop_cols if c in df.columns]) 
-        y = df[target_col]
-        return X, y
+    def get_features_and_target(df, target_col='monthly_excess_ret'):
+        drop_cols = ['DATE', 'year_month', 'permno', 'monthly_ret_raw', 'RF', target_col]
+        feature_cols = [c for c in df.columns if c not in drop_cols]
+        temp_df = df.dropna(subset=feature_cols + [target_col])
+
+        X = temp_df[feature_cols].values
+        y = temp_df[target_col].values
+
+        return X, y, temp_df
--- src/preprocess.py
+++ src/preprocess.py
@@ -4,23 +4,39 @@ import numpy as np
 
 class DataPipeline:
 
-    def __init__(self, filepath):
+    def __init__(self, filepath, filepath2):
         self.filepath = filepath
         self.df = None
-        self.indicator_matrix = None
+        self.risk_free_rate = filepath2
+        self.rfr = None
+        
 
-    def load(self):
+    def load_characteristics(self):
         self.df = pd.read_csv(self.filepath)
         self.df['DATE'] = pd.to_datetime(self.df['DATE'], errors='coerce', format='%Y%m%d')
         self.df['year_month'] = self.df['DATE'].dt.to_period('M')
         self.df = self.df.sort_values(['permno', 'DATE']).reset_index(drop=True)
-        self.df['ret'] = self.df.groupby('permno')['mom1m'].shift(-1)
+        self.df['monthly_ret_raw'] = self.df.groupby('permno')['mom1m'].shift(-1)
 
         print(f"Loaded {self.df.shape[0]} rows and {self.df.shape[1]} columns")
         return self
 
+    def load_risk_free_rate(self):
+        self.rfr = pd.read_csv(self.filepath2, skiprows=4)
+        self.rfr = self.rfr.rename(columns = {'Unnamed:0':'year_month'})
+        self.rfr['year_month'] = pd.to_datetime(self.rfr['year_month'], errors='coerce', format = '%Y%m').dt.to_period('M')
+        self.rfr = self.rfr.drop(columns=['Mkt-RF', 'SMB', 'HML'])
+        self.rfr['RF'] = self.rfr['RF'].astype(np.float32)/100
+        return self
+    
+    def clean_target(self):
+        self.df = pd.merge(self.df, self.rfr, on='year_month', how='inner')
+        self.df['monthly_excess_ret'] = self.df['monthly_ret_raw'] - self.df['RF']
+        return self
+
     def impute_missing(self):
-        num_cols = self.df.select_dtypes(include='number').columns
+        exclude = ['monthly_ret_raw', 'permno', 'monthly_excess_ret', 'RF']
+        num_cols = [c for c in self.df.select_dtypes(include='number').columns if c not in exclude]
 
         # fill all numeric columns at once with cross-sectional monthly median
         self.df[num_cols] = self.df[num_cols].fillna(
@@ -35,7 +51,7 @@ class DataPipeline:
         return self
 
     def normalize(self):
-        exclude = ['DATE', 'year_month', 'permno', 'ret']
+        exclude = ['DATE', 'year_month', 'permno', 'monthly_ret_raw', 'monthly_excess_ret', 'RF']
         num_cols = [c for c in self.df.select_dtypes(include='number').columns if c not in exclude]
 
         for col in num_cols:
